0c7a828c7824bafbbac3b89eac4bc274c33242ec
Evgeny <bevgenyd@gmail.com>
2023-09-14 14:37:25 +0300

    3 zadanie

--- src/data/home.js
+++ src/data/home.js
@@ -2,7 +2,7 @@ export default [
     {
         img: "/assest/images/home/main-page_content_item-18.3494ef81481ff449f679cacb36fa40d8.png",
         title: "Выпускной",
-        description: "Знаменательное событие в жизни каждого ребенка и родителя",
+        description: "йцук",
         path: "vyipusknoj"
     },
     {
@@ -14,25 +14,25 @@ export default [
     {
         img: "/assest/images/home/main-page_content_item-20.3494ef81481ff449f679cacb36fa40d8.png",
         title: "Новый год",
-        description: "Знаменательное событие в жизни каждого ребенка и родителя",
+        description: "ячсм",
         path: "novyij-god"
     },
     {
         img: "/assest/images/home/main-page_content_item-21.3494ef81481ff449f679cacb36fa40d8.png",
         title: "Промо мероприятия",
-        description: "Знаменательное событие в жизни каждого ребенка и родителя",
+        description: "фывавафы",
         path: "promo-meropriyatiya"
     },
     {
         img: "/assest/images/home/main-page_content_item-22.3494ef81481ff449f679cacb36fa40d8.png",
         title: "Корпоратив",
-        description: "Знаменательное событие в жизни каждого ребенка и родителя",
+        description: "ячсмцукцу",
         path: "korporativ"
     },
     {
         img: "/assest/images/home/main-page_content_item-23.3494ef81481ff449f679cacb36fa40d8.png",
         title: "Свадьба",
-        description: "Знаменательное событие в жизни каждого ребенка и родителя",
+        description: "джаовщовап",
         path: "svadba"
     },
 ]
